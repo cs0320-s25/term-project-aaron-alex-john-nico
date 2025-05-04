6bb25df24d511d62e041b3ff52395c13979cbf9f
NicoDiLullo <ndilullo@thacher.org>
2025-05-04 18:09:25 -0400

    feat: team bar

--- final/src/DraftBoard.tsx
+++ final/src/DraftBoard.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import Navbar from "./Navbar";
+import TeamBar from "./TeamBar";
 
 const DraftBoard: React.FC = () => {
     return(
         <div style={{ width: "100vw", height: "100vh", backgroundColor: "#000000" }}>
             <Navbar />
+            <TeamBar />
             <p>Hello, world!</p>
         </div>
     );
---
+++ final/src/TeamBar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamBar: React.FC = () => {
+    return (
+        <div
+      style={{
+        backgroundColor: "#262942",
+        width: "100vw",
+        height: "20vh",
+        flexShrink: 0,
+      }}
+    >
+    </div>
+    );
+};
+
+export default TeamBar;
