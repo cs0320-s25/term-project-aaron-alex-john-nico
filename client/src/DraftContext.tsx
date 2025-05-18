2d2e822135554845861c3bfc54054035cc9f9944
NicoDiLullo <ndilullo@thacher.org>
2025-05-18 17:12:02 -0400

    feat: drafting

--- client/src/DraftContext.tsx
+++ client/src/DraftContext.tsx
@@ -1,20 +1,43 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { Player } from "./PlayerCard";
+import { mockPlayers } from "./data/mockplayers";
 
 interface DraftContextType {
-  numTeams: number;
-  setNumTeams: (n: number) => void;
   draftPosition: number;
   setDraftPosition: (n: number) => void;
+  availablePlayers: Player[];
+  setAvailablePlayers: (players: Player[]) => void;
+  teamRosters: Player[][];
+  setTeamRosters: (rosters: Player[][]) => void;
+  numTeams: number;
+  setNumTeams: (n: number) => void;
 }
 
 const DraftContext = createContext<DraftContextType | undefined>(undefined);
 
 export const DraftProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [numTeams, setNumTeams] = useState(8);
-  const [draftPosition, setDraftPosition] = useState(1);
+  const [draftPosition, setDraftPosition] = useState(0);
+  const [availablePlayers, setAvailablePlayers] = useState<Player[]>(mockPlayers);
+  const [numTeams, setNumTeams] = useState<number>(4);
+  const [teamRosters, setTeamRosters] = useState<Player[][]>(Array.from({ length: 4 }, () => []));
+
+  useEffect(() => {
+    setTeamRosters(Array.from({ length: numTeams }, () => []));
+  }, [numTeams]);
 
   return (
-    <DraftContext.Provider value={{ numTeams, setNumTeams, draftPosition, setDraftPosition }}>
+    <DraftContext.Provider
+      value={{
+        draftPosition,
+        setDraftPosition,
+        availablePlayers,
+        setAvailablePlayers,
+        teamRosters,
+        setTeamRosters,
+        numTeams,
+        setNumTeams,
+      }}
+    >
       {children}
     </DraftContext.Provider>
   );
@@ -22,19 +45,8 @@ export const DraftProvider: React.FC<{ children: React.ReactNode }> = ({ childre
 
 export const useDraft = () => {
   const context = useContext(DraftContext);
-  if (!context) throw new Error("useDraft must be used within a DraftProvider");
+  if (!context) {
+    throw new Error("useDraft must be used within a DraftProvider");
+  }
   return context;
 };
-
-export interface TeamRoster {
-    qb: string;
-    rb1: string;
-    rb2: string;
-    wr1: string;
-    wr2: string;
-    te: string;
-    flex: string;
-    def: string;
-    k: string;
-    bench: string[]; // 5 bench players
-};  
