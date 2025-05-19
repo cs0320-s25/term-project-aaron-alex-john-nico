4d8f96345e92d0fbaf2a855886842e0a66015576
NicoDiLullo <ndilullo@thacher.org>
2025-05-19 08:45:41 -0400

    feat: predicted points are back

--- server/playerPicker.py
+++ server/playerPicker.py
@@ -146,4 +146,4 @@ full_results['season'] = 2024
 # Use this for final standings
 standings = final_standings_players(full_results)
 #turn_to_json(standings)
-
+print("CSV Headers:", data.columns.tolist())
